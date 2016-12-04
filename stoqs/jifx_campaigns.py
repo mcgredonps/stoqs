53b51ae5f5d3fadda4024932235681318e1d3b93
vagrant <vagrant@localhost.localdomain>
2016-12-04 08:26:47 +0000

    Lots of hacking to get closer to correct. Clean it up later.

---
+++ stoqs/jifx_campaigns.py
@@ -0,0 +1,18 @@
+# File: mbari_campaigns.py
+#
+# Create a symbolic link named campaigns.py to tell the Django server 
+# to serve these databases: ln -s mbari_campaigns.py campaigns.py.
+# The stoqs/loaders/load.py script uses the load commands associated
+# with each database to execute the load and record the provenance.
+# Execute 'stoqs/loaders/load.py --help' for more information.
+
+from collections import OrderedDict
+
+# Keys are database (campaign) names, values are paths to load script 
+# for each campaign starting at the stoqs/loaders directory.  The full 
+# path of 'stoqs/loaders/' is prepended to the value and then executed.
+
+# Name must match loader name in jifx directory
+campaigns = OrderedDict([
+    ('jifx_aug_2016',   'JIFX/loadCANON_september2010.py'),
+])
